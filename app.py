f15ed7e36e041e917654935e591e232c2b77388d
Ram Mihir P. <91376295+Swix-iE@users.noreply.github.com>
2025-01-15 17:23:18 +0530

    Update app.py
    added a display of lines, and fixed some other bugs. Made it more user friendly.

--- app.py
+++ app.py
@@ -1,99 +1,98 @@
-import datetime
-import random
+import datetime as dt
+import random as rn
 
-MAX_BET = 1000
-MIN_BET = 10
-LINES = 3
+MIN_DEPOSIT = 10
+MAX_DEPOSIT = 1000
 
-
-class Player:
+class Person:
     def __init__(self, first_name, last_name, birth_date):
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
         
     def get_details(self):
-        return f""" Haligtree Lounge welcomes you {self.first_name} {self.last_name}!
-    Wishing you great luck on your journey to win big!"""
+        return f"""
+    G'day {self.first_name} {self.last_name}!!!
+    Haligtree Lounge welcomes you. G'Luck on your winnings."""
         
-class SlotMachine(Player):
-    def __init__(self, first_name, last_name, birth_date, deposit, bet):
+    def parse_birth_date(self):
+        year, month, day = map(int, self.birth_date)
+        today = dt.date.today()
+        b_date = dt.date(year, month, day)
+        age = today.year - b_date.year - ((today.month, today.day) < (b_date.month, b_date.day))
+        return age
+    
+class SlotMachine(Person):
+    def __init__(self, first_name, last_name, birth_date):
         super().__init__(first_name, last_name, birth_date)
-        self.deposit = deposit
-        self.bet = bet
+        self.balance = 0
         
-    def check_age(self):
-        birth_date = datetime.date(int(birth_date[0]), int(birth_date[1]), int(birth_date[2]))
-        today = datetime.date.today()
-        curr_age = today - birth_date
-        if curr_age.days < 6570:
-            print("You are not eligible to play this game.")
+    def age_check(self):
+        age = self.parse_birth_date()
+        return age 
     
-    def get_deposit(self,deposit):
-        deposit = input("Enter the amount you want to deposit: $")
-        if deposit.isdigit():
-            self.deposit = int(deposit)
-            if self.deposit < MIN_BET:
-                print("Minimum deposit amount is $10.")
-            elif self.deposit > MAX_BET:
-                print("Maximum deposit amount is $1000.")
-            else: 
-                print(f"Your deposit amount is ${self.deposit}.")
-                
-    def get_bet(self, bet):
-        bet = input("Enter the amount you want to bet: $")
-        if bet.isdigit():
-            self.bet = int(bet)
-            if self.bet < MIN_BET:
-                print("Minimum bet amount is $10.")
-            elif self.bet > MAX_BET:
-                print("Maximum bet amount is $1000.")
-        return self.bet
-    
-    def get_balance(self):
-        self.balance = self.deposit - self.bet
+    def get_deposit(self):
+        while True:
+            deposit = input("Enter the amount of money you want to deposit: $")
+            if deposit.isdigit():
+                deposit = int(deposit)
+                if MIN_DEPOSIT <= deposit <= MAX_DEPOSIT:
+                    self.balance += deposit
+                    print(f"Deposit successful! Your balance is now ${self.balance}.")
+                    break
+                else:
+                    print(f"Please enter an amount between ${MIN_DEPOSIT} - ${MAX_DEPOSIT}.")
+            else:
+                print("Please enter a valid number.")
         return self.balance
-    
-    def bet_on_lines(self, first_line, second_line, third_line):
-        self.first_line = first_line
-        self.second_line = second_line
-        self.third_line = third_line
         
-        first_line = random.randint(0, 7)
-        second_line = random.randint(0, 7)
-        third_line = random.randint(0, 7)
+    def get_bet(self):
+        while True:
+            bet = input("Enter the amount you want to bet: $")
+            if bet.isdigit():
+                bet = int(bet)
+                if bet > self.balance:
+                    print("Insufficient balance. Please deposit more money or lower your bet.")
+                else:
+                    self.balance -= bet
+                    print(f"Bet accepted! You bet ${bet}. Remaining balance: ${self.balance}.")
+                    return bet
+            else:
+                print("Please enter a valid number.")
         
-        return first_line, second_line, third_line
-    
     def winning_conditions(self):
-        if self.first_line == self.second_line == self.third_line == 0:
-            print("Better luck next time!")
-            self.balance = self.deposit - self.bet
-            print(f"Your balance is ${self.balance}.")
-            choice = input("Do you wish to continue playing? Y/N:")
-            if choice == "Y":
-                self.get_bet()
-            else: 
-                print("Thank you for playing!")
-        elif self.first_line == self.second_line == self.third_line == 7:
-            print("You hit the jackpot!")
-            self.balance = self.balance + self.bet * 10
+        bet = self.get_bet()
+        
+        first_line = rn.randint(0,7)
+        second_line = rn.randint(0,7)
+        third_line = rn.randint(0,7)
+        print(f"Slot results: {first_line} | {second_line} | {third_line}")
+        if first_line == second_line == third_line == 0:
+            print("Better luck next time :(")
+        elif first_line == second_line == third_line == 7:
+            available_balance = bet * 10
+            self.balance += available_balance
+            print(f"Congratulations, you hit the jackpot! You won ${available_balance}.")
         else:
-            print("better luck next time!")
-            
+            print("Better luck next time:(")
+        
+        print(f"Your available balance is ${self.balance}.")
+        
+
 
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
-birth_date = input("Enter your birth date in 'YYYY-MM-DD' format seperated with spaces: ").split()
+birth_date = input("Enter your date of birth in 'YYYY-MM-DD' format: ").split(" ")
 
-p = Player(first_name, last_name, birth_date)
-print(p.get_details())
-deposit = input("Enter the amount you want to deposit: $")
-bet = input("Enter the amount you want to bet: $")
-slot = SlotMachine(first_name, last_name, birth_date, deposit, bet)
-slot.check_age()
-slot.get_deposit(deposit)
-slot.get_bet(bet)
-slot.get_balance()
-slot.bet_on_lines()
-slot.winning_conditions()
+person = Person(first_name, last_name, birth_date)
+print(person.get_details())
+
+slot = SlotMachine(first_name, last_name, birth_date)
+age = slot.age_check()
+
+if age >= 18:
+    print("You are eligible to play!")
+    slot.get_deposit()
+    slot.winning_conditions()
+else:
+    print(f"You are not eligible to play as your age ({age}) is less than the minimum required age.")
